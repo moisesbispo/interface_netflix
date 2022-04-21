be8a7103e07f854b892ae121909a9d9c76c0e3f2
Moisés Bispo <moisesbbispo@gmail.com>
2022-04-21 17:46:59 -0300

    parte final do desafio (zoom e alteração dinâmica de itens)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # interface_netflix
 Desafio de recriar a interface do Netflix - DIO(bootcamp carrefour)
+
+Além das funcionalidades do desafio, foi adicionado um zoom de destaque ao passar o mouse por algum filme ou série e também alterar imagem e textos do destaque ao estar com o mouse sobre algum item do carousel.
--- js/owl/setup.js
+++ js/owl/setup.js
@@ -14,3 +14,126 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+function entrar1() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo1.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "VIKINGS VALHALLA"
+    descricao.innerText = "Nesta sequência de Vikings, cem anos se passaram. Uma nova geração de heróis lendários cria seu destino e faz história."
+}
+
+function entrar2() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo2.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "A ANATOMIA DA GREY"
+    descricao.innerText = "Os médicos do Grey Sloan Memorial Hospital lidam diariamente com casos e consequências de vida ou morte. É um no outro que eles encontram apoio, conforto, amizade e, às vezes, até mais que amizade... Juntos, eles descobrem o quanto a vida profissional e a pessoal podem ser complicadas e se misturarem no meio do caminho."
+}
+
+function entrar3() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo3.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "HOMEM ARANHA: SEM VOLTA PRA CASA"
+    descricao.innerText = "Peter Parker é desmascarado e não consegue mais separar sua vida normal dos grandes riscos de ser um super-herói. Quando ele pede ajuda ao Doutor Estranho, os riscos se tornam ainda mais perigosos, e o forçam a descobrir o que realmente significa ser o Homem-Aranha."
+}
+
+function entrar4() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo4.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "O PROJETO ADAM"
+    descricao.innerText = "Depois de viajar no tempo e ir parar em 2022 sem querer, o piloto de combate Adam Reed se une à sua versão de 12 anos de idade para salvar o futuro."
+}
+
+function entrar5() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo5.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "SONIC: O FILME"
+    descricao.innerText = "Sonic, o ouriço azul mais veloz do mundo, vive isolado e sem amigos na Terra desde que precisou fugir de seu planeta natal. Todavia, ele recebe a ajuda de um policial quando o Dr. Ivo Robotinik, a mando do governo dos Estados Unidos, começa a persegui-lo."
+}
+
+function entrar6() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo6.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "ETERNOS"
+    descricao.innerText = "Após os eventos de Vingadores: Ultimato, os Eternos, uma raça alienígena imortal criada pelos Celestiais que vivem em segredo na Terra há mais de 7000 anos, se reúnem após uma tragédia inesperada para proteger a humanidade de seus colegas malignos: os Deviantes."
+}
+
+function entrar7() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo7.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "EUPHORIA"
+    descricao.innerText = "Um grupo de estudantes do ensino médio lida com diferentes situações típicas da idade como drogas, sexo, busca pela identidade, traumas, comportamento nas redes sociais e amizade."
+}
+
+function entrar8() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo8.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "BATMAN"
+    descricao.innerText = "Após dois anos espreitando as ruas como Batman e aterrorizando o mundo do crime, Bruce Wayne se encontra nas profundezas mais sombrias de Gotham City. Com pouquíssimos aliados confiáveis dentro da rede corrupta de autoridades e grandes figuras da cidade, o vigilante solitário se estabeleceu como a personificação da vingança para a população.Agora, quando um assassino tem a elite de Gotham vítima de uma série de tramas sadistas, a trilha de pistas crípticas leva o Maior Detetive do Mundo em uma investigação no submundo, onde encontra Carmine Falcone, Selina Kyle, vulga Mulher-Gato, Oswald Cobblepot, vulgo Pinguim, e Edward Nashton, vulgo Charada. Conforme as evidências atingem um nível pessoal e a dimensão dos planos do responsável se torna mais clara, o Batman deve forjar novas alianças, desmascarar o culpado e trazer justiça ao abuso de poder e corrupção que a tanto tempo atormenta Gotham City."
+}
+
+function entrar9() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo9.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "KING'S MAN: A ORIGEM"
+    descricao.innerText = "Quando uma série dos piores tiranos e gênios do crime da história se juntam para criar uma guerra que aniquilará milhões, um homem e seu pupilo precisam correr contra o tempo para pará-los."
+}
+
+function entrar10() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/fundo10.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "CAVALEIRO DA LUA"
+    descricao.innerText = "A série acompanha Steven Grant, um gentil e educado funcionário de uma loja de souvenir, que é atormentado com apagões e memórias de outra vida. Steven descobre que tem transtorno dissociativo de identidade e divide o corpo com o mercenário Marc Spector. À medida que os inimigos de Steven/Marc se voltam para eles, ambos devem navegar em suas complexas identidades enquanto mergulham em um mistério mortal entre os poderosos deuses do Egito."
+}
+
+function sair() {
+    var titulo = document.getElementsByClassName("titulo")[0]
+    var descricao = document.getElementsByClassName("descricao")[0]
+    
+    $("#filme-principal").css("background", "url(/img/capa-peaky.jpg)"); 
+    $("#filme-principal").css("background-size", "cover"); 
+    
+    titulo.innerText = "PEAKY BLINDERS"
+    descricao.innerText = "Thomas Shelby e seus irmãos retornam a Birmingham depois de servir no exército britânico durante a Primeira Guerra Mundial. Os Peaky Blinders, a gangue na qual Thomas é líder, controlam a cidade de Birmingham. Mas, como as ambições de Shelby se estendem para além de Birmingham, ele planeja construir o império de negócios que criou e impedir qualquer um que atrapalhar."
+}
+
